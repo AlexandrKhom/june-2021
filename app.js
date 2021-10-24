fe1bbeb18b8522b55b6a20e66da6e37ed7e524eb
AlexandrKhom <alex988andr@gmail.com>
2021-10-25 02:57:59 +0300

    2

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
-// const user = require('./helper/user-builder')
-// console.log(222)
-// user.createUser('alex', 22).greeting()
+const express = require('express');
 
-const fs = require('fs')
-const path = require('path')
+const app = express();
 
-// fs.readFile(`${__dirname}/files/test.txt`, ((err, data)=> {
-//   if (err){
-//     console.log(err)
-//     return
-//   }
-//   console.log(data.toString())
-// }))
+app.get('/users', (req, res)=> {
+  res.json('hello world get user')
+})
 
-// fs.writeFile(`${__dirname}/files/test.txt`, 'hello', (err)=> {
-//   if (err){
-//     console.log(err)}
-// })
+app.post('/users', (req, res)=> {
+  res.json('hello world post user')
+})
 
-// fs.appendFile(`${__dirname}/files/test.txt`, 'hello', (err)=> {
-//   if (err){
-//     console.log(err)}
-// })
-//
-// console.log(__dirname);
-// console.log(__filename);
+app.put('/users', (req, res)=> {
+  res.json('hello world update user')
+})
 
-// const pathFile = path.join(__dirname, 'files', 'append.txt')
-// console.log(pathFile)
+app.listen(5000, ()=> {
+  console.log('App listen 5000')
+})
 
-// const mkdirPath = path.join(__dirname, 'files', 'test', 'user')
-//
-// fs.mkdir(mkdirPath, {recursive: true}, err => {
-//   console.log(err)
-// })
 
-// fs.unlink(`${__dirname}/files/test/user/user.js`, err => {
-//   console.log(err)
-// })
 
-fs.rename(`${__dirname}/files/test/user/user.js`,
-  path.join(__dirname, 'helper', 'userNew'),
-  err => {
-    console.log(err)
-  })
+
+
 
