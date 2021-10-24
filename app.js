1687041f1f548ca8fac5e726b7ced60cb6d79aec
AlexandrKhom <alex988andr@gmail.com>
2021-10-25 01:34:57 +0300

    1

--- app.js
+++ app.js
@@ -5,29 +5,43 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.readFile(`${__dirname}/files/test.txt`, ((err, data)=> {
-  if (err){
-    console.log(err)
-    return
-  }
-  console.log(data.toString())
-}))
+// fs.readFile(`${__dirname}/files/test.txt`, ((err, data)=> {
+//   if (err){
+//     console.log(err)
+//     return
+//   }
+//   console.log(data.toString())
+// }))
 
 // fs.writeFile(`${__dirname}/files/test.txt`, 'hello', (err)=> {
 //   if (err){
 //     console.log(err)}
 // })
 
-fs.appendFile(`${__dirname}/files/test.txt`, 'hello', (err)=> {
-  if (err){
-    console.log(err)}
-})
+// fs.appendFile(`${__dirname}/files/test.txt`, 'hello', (err)=> {
+//   if (err){
+//     console.log(err)}
+// })
+//
+// console.log(__dirname);
+// console.log(__filename);
 
-console.log(__dirname);
-console.log(__filename);
+// const pathFile = path.join(__dirname, 'files', 'append.txt')
+// console.log(pathFile)
 
+// const mkdirPath = path.join(__dirname, 'files', 'test', 'user')
+//
+// fs.mkdir(mkdirPath, {recursive: true}, err => {
+//   console.log(err)
+// })
 
+// fs.unlink(`${__dirname}/files/test/user/user.js`, err => {
+//   console.log(err)
+// })
 
-
-
+fs.rename(`${__dirname}/files/test/user/user.js`,
+  path.join(__dirname, 'helper', 'userNew'),
+  err => {
+    console.log(err)
+  })
 
