8b0eb8d282aeebf85c75b7825506a392b077c999
AlexandrKhom <alex988andr@gmail.com>
2021-10-26 03:37:56 +0300

    3

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -1,10 +1,16 @@
+const User = require('../dataBase/User')
+
 module.exports = {
-  createUserMiddleware: (req, res, next) => {
+  createUserMiddleware: async (req, res, next) => {
     try {
       console.log('MIDDLEWARE WORK')
+      const userByEmail = await User.findOne({email: req.body.email})
+      if (userByEmail) {
+        throw new Error('email is exist')
+      }
       next()
     } catch (e) {
-      console.log(e)
+      console.log(e.message)
     }
   }
 }
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller')
 const userMIddleware = require('../middlewares/user.middleware')
 
-router.get('/', userMIddleware.createUserMiddleware, userController.getUser);
+router.get('/', userController.getUser);
 
 router.get('/:user_id', userController.getUserById);
 
-router.post('/', userController.createUser);
+router.post('/', userMIddleware.createUserMiddleware, userController.createUser);
 
 router.put('/', userController.updateUser);
 
