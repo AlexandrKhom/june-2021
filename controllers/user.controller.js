0b2ba5c99d80bd31b72f5b160a8a3897a315e411
AlexandrKhom <alex988andr@gmail.com>
2021-10-29 22:43:11 +0300

    1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,27 @@
-const db = require('../dataBase/users')
-
-module.exports = {
-  getUsers: (req, res) => {
-    res.json(db)
-  },
-  getUserById: (req, res) => {
-    const { user_id } = req.params
-    const user = db[user_id - 1]
-    res.json(user)
-  },
-  postUsers: (req, res) => {
-    db.push({ ...req.body, id: db.length + 1 })
-    res.json(db)
-  },
-  deleteUsers: (req, res) => {
-    res.json('Delete Users')
-  }
-}
+// //запрос в локальную базу данных
+// const db = require('../dataBase/users')
+//
+// //модуль для экспорта
+// module.exports = {
+// //контроллер для получения всех юзеров
+//   getUsers: (req, res) => {
+//     res.json(db)
+//   },
+// //для получения 1 юзера
+//   getUserById: (req, res) => {
+//     //в req.params то, что мы передаём в роутере после "/" (/:user_id)
+//     const { user_id } = req.params
+//     // поиск юзера в базе по индексу
+//     const user = db[user_id - 1]
+//     res.json(user)
+//   },
+// //создаём юзера
+//   postUsers: (req, res) => {
+//     // в req.body то, что мы написали в постмане
+//     db.push({ ...req.body, id: db.length + 1 })
+//     res.json(db)
+//   },
+// }
 
 
 
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,11 +1,16 @@
-const router = require('express').Router()
-const userController = require('../controllers/user.controller')
-
-router.post(`/`, userController.postUsers)
-router.get(`/`, userController.getUsers)
-router.get(`/:user_id`, userController.getUserById)
-router.delete(`/`, userController.deleteUsers)
-
-module.exports = router
+// //создаём роутер (из экспресса)
+// const router = require('express').Router()
+// //создаём контроллер из файла с контроллером
+// const userController = require('../controllers/user.controller')
+//
+// //роутер для создания
+// router.post(`/`, userController.postUsers)
+// //роутер для получения всех
+// router.get(`/`, userController.getUsers)
+// //роутер для получения одного
+// router.get(`/:user_id`, userController.getUserById)
+//
+// //экспортируем
+// module.exports = router
 
 
