73d625fe908b019ec8aa9cbdaa3d391d717cbd45
AlexandrKhom <alex988andr@gmail.com>
2021-11-04 23:56:30 +0200

    1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,121 +1,19 @@
-// //запрос в локальную базу данных
-// const db = require('../dataBase/users')
-//
-// //модуль для экспорта
-// module.exports = {
-// //контроллер для получения всех юзеров
-//   getUsers: (req, res) => {
-//     res.json(db)
-//   },
-// //для получения 1 юзера
-//   getUserById: (req, res) => {
-//     //в req.params то, что мы передаём в роутере после "/" (/:user_id)
-//     const { user_id } = req.params
-//     // поиск юзера в базе по индексу
-//     const user = db[user_id - 1]
-//     res.json(user)
-//   },
-// //создаём юзера
-//   postUsers: (req, res) => {
-//     // в req.body то, что мы написали в постмане
-//     db.push({ ...req.body, id: db.length + 1 })
-//     res.json(db)
-//   },
-// }
-
-// const db = require('../dataBase/users')
-
-
-//
-// //импортируем модель схемы из базы
-// const User = require('../dataBase/User')
-//
-// const passwordService = require('../service/password.service')
-// const userUtil = require('../util/user.util')
-//
-// //все запросы должны быть асинхронными и в try/catch
-// module.exports = {
-//   getUsers: async (req, res)=> {
-//     try {
-//       //у модели появляются методы MongoDB
-//       const users = await User.find()
-//       res.json(users)
-//     } catch (e) {
-//       res.json(e)
-//     }
-//   },
-//
-//   //при получении юзера скрываем пароль
-//   getUsersById: async (req, res)=> {
-//     try {
-//       const {user_id} = req.params
-//       let user = await User.findById(user_id).lean()
-//       user = userUtil.userNormalizator(user)
-//       res.json(user)
-//     } catch (e) {
-//       res.json(e)
-//     }
-//   },
-//
-//   //при создании юзера хешируем пароль
-//   createUser: async (req, res) => {
-//    try {
-//      const hashedPassword = await passwordService.hash(req.body.password)
-//      const newUser = await User.create({...req.body, password: hashedPassword})
-//      res.json(newUser)
-//    } catch (e) {
-//      res.json(e)
-//    }
-//   },
-//   deleteUser: async (req, res) => {
-//     try {
-//       const {user_id} = req.params
-//       const user = await User.findOneAndDelete(user_id)
-//       res.json(user)
-//     }catch (e) {
-//       console.log(e)
-//     }
-//   }
-// }
-
-
-const User = require('../dataBase/User')
+const db = require('../dataBase/users')
 
 module.exports = {
-  getUsers: async (req, res)=> {
-   try {
-     const user = await User.find()
-     res.json(user)
-   }catch (e) {
-     console.log(e)
-   }
+  getUsers: (req, res) => {
+    res.json(db)
   },
-  getUsersById: async (req, res)=> {
-   try {
-     const {user_id} = req.params
-     const user = await User.findById(user_id)
-     res.json(user)
-   }catch (e) {
-     console.log(e)
-   }
+  getUser: (req, res) => {
+    const {id} = req.params
+    const user = db[id - 1]
+    res.json(user)
   },
-  postUsers: async (req, res)=> {
-    try {
-      const  user = await User.create(req.body)
-      res.json(user)
-    }catch (e) {
-      console.log(e)
-    }
-  },
-  deleteUsersById: async (req, res)=> {
-    try {
-      const {user_id} = req.params
-      const user = await User.findByIdAndDelete(user_id)
-      res.json(user)
-    }catch (e) {
-      console.log(e)
-    }
-  },
+  postUser: (req, res)=> {
+    const user = {...req.body, id: db.length + 1}
+    db.push(user)
+    res.json(user)
+  }
 }
 
 
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,37 +1,9 @@
-// //создаём роутер (из экспресса)
-// const router = require('express').Router()
-// //создаём контроллер из файла с контроллером
-// const userController = require('../controllers/user.controller')
-//
-// //роутер для создания
-// router.post(`/`, userController.postUsers)
-// //роутер для получения всех
-// router.get(`/`, userController.getUsers)
-// //роутер для получения одного
-// router.get(`/:user_id`, userController.getUserById)
-//
-// //экспортируем
-// module.exports = router
-
-
-// const router = require('express').Router()
-// const userController = require('../controllers/user.controller')
-// const userMiddleware = require('../middlewares/user.middleware')
-// //все мидлвары добавляем ДО контроллера
-// router.post('/', userMiddleware.isUserValid, userMiddleware.createUserMiddleware, userController.createUser)
-// router.get('/', userController.getUsers)
-// router.get('/:user_id', userController.getUsersById)
-// router.delete('/:user_id', userController.deleteUser)
-//
-// module.exports = router
-
-
-const router = require('express').Router()
+const {Router} = require('express')
+const router = Router()
 const userController = require('../controllers/user.controller')
 
-router.get('/', userController.getUsers);
-router.get('/:user_id', userController.getUsersById);
-router.post('/', userController.postUsers);
-router.delete('/:user_id', userController.deleteUsersById)
+router.get('/', userController.getUsers)
+router.get('/:id', userController.getUser)
+router.post('/', userController.postUser)
 
 module.exports = router
